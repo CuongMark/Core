fe010339f5cfb3eef06f92aef099435939ef5e04
mark <cuonglv53@gmail.com>
2019-06-08 20:11:32 +0700

    Random Number Gennerated

---
+++ app/code/Angel/Core/Model/RandomNumberGenerate.php
@@ -0,0 +1,65 @@
+<?php
+namespace Angel\Core\Model;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Phrase;
+
+class RandomNumberGenerate
+{
+    /**
+     * Return a random number in the specified range
+     *
+     * @param int $min
+     * @param int $max
+     * @return int  A random integer value between min (or 0) and max
+     * @throws LocalizedException
+     * @throws \Exception
+     */
+    public static function getWinningNumber($min = 0, $max = null)
+    {
+        if (null === $max) {
+            $max = mt_getrandmax();
+        }
+
+        if ($max < $min) {
+            throw new LocalizedException(new Phrase('Invalid range given.'));
+        }
+
+        return random_int($min, $max);
+    }
+
+    /**
+     * @param array $drawnCard
+     * @return int
+     * @throws LocalizedException
+     */
+    public static function drawCard($drawnCard = []){
+        $drawnCard = array_unique($drawnCard);
+        if (count($drawnCard) >= 54){
+            throw new LocalizedException(new Phrase('Invalid Drawn Card.'));
+        }
+
+        $cardNumber = self::getWinningNumber(1, 54);
+        while (in_array($cardNumber, $drawnCard)){
+            $cardNumber = self::getWinningNumber(1, 54);
+        }
+        return $cardNumber;
+    }
+
+    /**
+     * @param int $start
+     * @param int $end
+     * @param array $existed
+     * @return int
+     * @throws \Exception
+     */
+    public static function getRandomNumber($start, $end, &$existed){
+        $number = random_int($start, $end);
+        /** To make sure the winning numbers are not duplicated */
+        while (in_array($number, $existed)){
+            $number = random_int($start, $end);
+        }
+        $existed[] = $number;
+        return $number;
+    }
+}
